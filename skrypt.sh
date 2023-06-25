d64577b96cf934a7f2ccf62b4cd28d367f488917
Adasieq01 <salapa.adam@gmail.com>
2023-06-25 11:00:31 +0200

    new --logs

--- skrypt.sh
+++ skrypt.sh
@@ -3,4 +3,13 @@ if [ $1 == "--date" ]
 then
 echo "date: $(date)"
 fi
+if [$1 == "--logs"]
+then
+for((i=1;i<101;i++)); do
+filename="log$i.txt"
+scriptname=$0
+echo "Name: $filename" > "$filename"
+echo "Script: $scriptname" > "$filename"
+echo "Date: $(date)" > "$filename"
+done
 
